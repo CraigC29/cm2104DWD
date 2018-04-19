f7db133da8a5b084c77bb6327573eae3efed6867
Craig Cargill <craigc29@btinternet.com>
2018-04-20 00:46:11 +0100

    changed users to profile

--- lab18-profilesystem/server.js
+++ lab18-profilesystem/server.js
@@ -47,7 +47,7 @@ app.get('/', function(req, res) {
   db.collection('people').find().toArray(function(err, result) {
     if (err) throw err;
     //the result of the query is sent to the users page as the "users" array
-    res.render('pages/users', {
+    res.render('pages/profile', {
       users: result
     })
   });
