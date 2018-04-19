25c419023a328b20d6e7561f4ad7886d9f460f92
Craig Cargill <craigc29@btinternet.com>
2018-04-19 01:48:59 +0100

    added filter

--- lab15/server.js
+++ lab15/server.js
@@ -7,30 +7,44 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended:true}))
 var db;
 MongoClient.connect(url, function(err, database){
- if(err) throw err;
- db = database;
- app.listen(8080);
+  if(err) throw err;
+  db = database;
+  app.listen(8080);
 });
 
 app.get('/all', function(req, res) {
- db.collection('quotes').find().toArray(function(err, result) {
- if (err) throw err;
- var output = "<h1>All the quotes</h1>";
- for (var i = 0; i < result.length; i++) {
- output += "<div>"
- output += "<h3>" + result[i].name + "</h3>"
- output += "<p>" + result[i].quote + "</p>"
- output += "</div>"
- }
- res.send(output);
- });
+  db.collection('quotes').find().toArray(function(err, result) {
+    if (err) throw err;
+    var output = "<h1>All the quotes</h1>";
+    for (var i = 0; i < result.length; i++) {
+      output += "<div>"
+      output += "<h3>" + result[i].name + "</h3>"
+      output += "<p>" + result[i].quote + "</p>"
+      output += "</div>"
+    }
+    res.send(output);
+  });
 });
 
 
 app.post('/quotes', function (req, res) {
- db.collection('quotes').save(req.body, function(err, result) {
- if (err) throw err;
- console.log('saved to database')
- res.redirect('/')
- })
+  db.collection('quotes').save(req.body, function(err, result) {
+    if (err) throw err;
+    console.log('saved to database')
+    res.redirect('/')
+  })
 })
+
+app.post('/search', function(req, res) {
+  db.collection('quotes').find(req.body).toArray(function(err, result) {
+    if (err) throw err;
+    var output = "<h1>All the quotes</h1>";
+    for (var i = 0; i < result.length; i++) {
+      output += "<div>"
+      output += "<h3>" + result[i].name + "</h3>"
+      output += "<p>" + result[i].quote + "</p>"
+      output += "</div>"
+    }
+    res.send(output);
+  });
+});
